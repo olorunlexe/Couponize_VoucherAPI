1a824f35b0d68e57d32247e5053434ea2dd86820
Enunwah Stephen <senunwah@yahoo.com>
2018-12-03 10:16:25 +0100

    Edited Voucher

--- VoucherDependencies/Domain/Voucher.cs
+++ VoucherDependencies/Domain/Voucher.cs
@@ -1,18 +1,19 @@
 ﻿using System;
+using VoucherDependencies.Util;
 
 namespace VoucherDependencies.Domain
 {
     public class Voucher 
     {
         public string Code { get; }
-        public string Voucher_type { get; }
+        public Voucher_Type Voucher_type { get; }
         public string Category{ get; }
         public string Additional_Info{ get; }
         public DateTime Start_Date{ get; }
         public DateTime Expiration_Date{ get; }
         public bool Active{ get; }
 
-        public Voucher(string code, string voucher_type, string category, string additional_Info, DateTime start_Date, DateTime expiration_Date, bool active)
+        public Voucher(string code, Voucher_Type voucher_type, string category, string additional_Info, DateTime start_Date, DateTime expiration_Date, bool active)
         {
             this.Code = code;
             this.Voucher_type = voucher_type;
