565450a782bf47ea3b2753eaa11cf9069ee062af
Enunwah Stephen <senunwah@yahoo.com>
2018-12-04 14:46:26 +0100

    Fixed Create Response Beans

--- VoucherDependencies/Api/GetVoucherRequest.cs
+++ VoucherDependencies/Api/GetVoucherRequest.cs
@@ -12,30 +12,15 @@ namespace VoucherDependencies.Api
 {
     public class GetVoucherRequest
     {
-        static string strConnectionString = "User Id=sa;Password=Deolu007@;Database=VoucherTest;";
+        string strConnectionString = "User Id=sa;Password=Deolu007@;Database=VoucherTest;";
         public static async Task<VoucherModel> GetVoucherAsync(string code)
         {
             try
             {
-                using(IDbConnection conn = new SqlConnection(strConnectionString))
-                {
-                    if (conn.State == ConnectionState.Closed)
-                    {
-                        conn.Open();
-                    }
-
-                    DynamicParameters parameters = new DynamicParameters();
-                    parameters.Add("@code", code);
-
-                    IDataReader reader = await conn.ExecuteReaderAsync("GetVoucher",parameters,commandType: CommandType.StoredProcedure);
-                    while (reader.Read())
-                    {
-                        
-                    }
-                    VoucherModel get = new VoucherModel();
-                    get.Code = code;
-                    return get;
-                }
+                
+                VoucherModel get = new VoucherModel();
+                get.Code = code;
+                return get;
             }
             catch (Exception e)
             {
